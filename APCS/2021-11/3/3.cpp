b5049087bb63fd353b289243a359cbac36b80e9c
nonoivan1234 <nonoivan0627@gmail.com>
2022-01-08 13:27:26 +0800

    Update 3.cpp

--- APCS/2021-11/3/3.cpp
+++ APCS/2021-11/3/3.cpp
@@ -1,25 +1,42 @@
 #include<bits/stdc++.h>
 using namespace std;
 #define nono_is_handsome cin.tie(0); ios_base::sync_with_stdio(0);
-#define N 200000
+#define N 200001
+#define int long long
 
-int w[N], t[N], r[N], l[N], prefixSum[N];
+int w, r, l, load[N]={0}, t[N], total=0;
 
-int main(){
+bool cmp(int i, int j){
+    return i>j;
+}
+
+signed main(){
     nono_is_handsome
 
     int n,m;
     cin>>n>>m;
-    for(int i=0;i<m;i++){
-        cin>>l[i]>>r[i]>>w[i];
+
+    for (int i=0;i<m;i++){
+        cin>>l>>r>>w;
+        load[l] += w;
+        load[++r] -= w;
     }
+
+    for (int i=1;i<=n;i++){
+        load[i] += load[i-1];
+    }
+
     for(int i=0;i<n;i++){
         cin>>t[i];
     }
-    prefixSum[0] = t[0];
-    for(int i=1;i<n;i++){
-        prefixSum[i] = t[i]+prefixSum[i-1];
+    sort(load, load+n+1, cmp);
+    sort(t, t+n);
+    
+    for (int i=0;i<n;i++){
+        total += t[i]*load[i];
     }
-    
-    
+
+    cout<<total<<"\n";
+
+    return 0;
 }
